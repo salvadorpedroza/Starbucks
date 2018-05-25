98a8c57914419054f6b4c09f729a0d8ebbc99354
salvadorpedroza <salvador.pedroza@outlook.es>
2018-05-24 20:38:11 -0500

    size-fix

--- my-app/src/app/caja/caja.component.ts
+++ my-app/src/app/caja/caja.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./caja.component.css']
 })
 export class CajaComponent implements OnInit {
-
-  constructor() { }
+  menuItems: any[];
+  constructor() {
+    this.menuItems = [
+      { path: '../../assets/images/chico.jpg', title: 'CHICO $20' },
+      { path: '../../assets/images/mediano.jpg', title: 'MEDIANO $35' },
+      { path: '../../assets/images/grande.jpg', title: 'GRANDE $49' },
+      { path: '../../assets/images/jumbo.jpg', title: 'JUMBO $59' }
+    ]
+   }
 
   ngOnInit() {
   }
